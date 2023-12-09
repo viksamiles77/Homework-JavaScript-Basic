d7d24ad42721b0fe033cada33d13292e72ef7815
viksamiles77 <mileskiviktor77@gmail.com>
2023-12-09 01:17:33 +0100

    hw2

--- Homework 2/script.js
+++ Homework 2/script.js
@@ -3,6 +3,7 @@ const year = parseInt(yearInputAsString); //converting the string to a number
 
 if (isNaN(year)) { //checking if the input is a valid number
     console.log('Enter a valid number.'); //if not valid
+    alert('Enter a valid number');
 } else { //if valid
     const allZodiacSigns = ['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig']; //every animal(element) has an index in the array
     const zodiacFormula = (year - 4) % 12; //calculate the index
