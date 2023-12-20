e971b9ffe4e5b66fb7c796cf899cff50fe6750a5
viksamiles77 <mileskiviktor77@gmail.com>
2023-12-21 00:13:39 +0100

    hw5

--- Homework 5/hw 5 part 1/script.js
+++ Homework 5/hw 5 part 1/script.js
@@ -1,24 +1,20 @@
-let firstDiv = document.querySelector('#first'); //first div selection
+let heading1 = document.getElementById('myTitle'); //first title selection
+heading1.innerText = 'First heading is changed'; //first title text change
 
-let firstTitle = document.querySelector('#first #myTitle'); //first title selection
-firstTitle.innerText = 'First title text changed'; //first title text change
+let paragraph1 = document.querySelector('.paragraph'); //first paragraph selection
+paragraph1.innerText = 'First paragraph is changed'; //first paragraph text change
 
-let firstParagraph = document.querySelector('#first .paragraph'); //first paragraph selection
-firstParagraph.innerText = 'First paragraph text changed'; //first paragraph text change
+let paragraph2 = document.querySelector('.second'); //second paragraph selection
+paragraph2.innerText = 'Second paragraph changed'; //second paragraph text change
 
-let secondDiv = document.querySelector('anotherDiv'); //second div selection
+let paragraph3 = document.querySelector('text'); //third paragraph selection
+paragraph3.innerText = 'Third paragraph is changed'; //third paragraph text change
 
-let secondParagraph = document.querySelector('.anotherDiv .second'); //second paragraph selection
-secondParagraph.innerText = 'Second paragraph text changed'; //second paragraph text change
+let heading0 = document.getElementsByTagName('h1'); //heading0 selects all h1 tags
+let heading2 = heading0[1]; //second heading selection with index1
+heading2.innerText = 'Second heading is changed'; //second heading text change
 
-let thirdParagraph = document.querySelector('.anotherDiv text'); //third paragraph text change
-thirdParagraph.innerText = 'Third paragraph text changed'; //third paragraph text change
-
-// let secondTitle = document.querySelector(''); 
-// secondTitle.innerText = 'Second title text changed';
-
-let headerThree = document.querySelector('h3');
-headerThree.innerText = 'h3 text changed'
+let heading3 = document.querySelector('h3'); //third heading selection
+heading3.innerText = 'Third heading is changed'; //third heading text change
 
 
-// imam malku nejasno bidejki ne bev na cas, so selektiranje na second title, mi gi menuva i drugite h1 elementi, ne uspeav i so children selector
