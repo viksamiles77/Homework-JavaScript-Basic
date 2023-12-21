c3373e6bb121a1b0064b7772c0c7577e5c51e18c
viksamiles77 <mileskiviktor77@gmail.com>
2023-12-21 01:28:17 +0100

    hw6

---
+++ Homework 6/script.js
@@ -0,0 +1,25 @@
+function createTable(rows, cols) {
+    let body = document.getElementsByTagName('body')[0]; // selecting the body
+    let table = document.createElement('table'); // creating table element
+    let tableBody = document.createElement('tbody'); // creating table body
+
+    for (let i = 1; i <= rows; i++) { // creating rows
+        let row = document.createElement('tr'); // creating table row
+
+        for (let j = 1; j <= cols; j++) { // creating cols
+            let cell = document.createElement('td'); // creating table cell
+
+            cell.appendChild(document.createTextNode('Row ' + i + ' Col ' + j)); // cell content
+            row.appendChild(cell);
+        }
+
+        tableBody.appendChild(row);
+
+    }
+
+    table.appendChild(tableBody);
+    body.appendChild(table);
+
+}
+
+createTable(5, 5);
