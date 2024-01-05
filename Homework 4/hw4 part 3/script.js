1d573d25428d425b0d58b234786f88e59a731923
viksamiles77 <mileskiviktor77@gmail.com>
2024-01-05 23:57:24 +0100

    hw4

--- Homework 4/hw4 part 3/script.js
+++ Homework 4/hw4 part 3/script.js
@@ -1,7 +1,12 @@
-function createAndReturnBigString() {
-    const elements = ['Java', 'script', 'is', 'fun', 'to', 'learn'];
-    const fullSaying = `${elements[0]} ${elements[1]} ${elements[2]} ${elements[3]} ${elements[4]} ${elements[5]}.`
-    console.log(fullSaying);
+const words = ['Java', 'script', 'is', 'fun', 'to', 'learn'];
+let sentence = '';
+
+for (let i = 0; i < words.length; i++) {
+    sentence += words[i];
+
+    if (i < words.length - 1) {
+        sentence += ' ';
+    }
 }
 
-createAndReturnBigString()
+console.log(sentence);
